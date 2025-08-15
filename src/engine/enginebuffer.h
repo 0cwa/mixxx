3634267c3dbe39629658e423a52aa4083766a0b9
numtel <ben@latenightsketches.com>
2025-08-15 13:20:32 -0700

    Seek30

--- src/engine/enginebuffer.h
+++ src/engine/enginebuffer.h
@@ -40,6 +40,7 @@ class VinylControlControl;
 class LoopingControl;
 class ClockControl;
 class CueControl;
+class Seek30Control;
 class ReadAheadManager;
 class ControlObject;
 class ControlProxy;
@@ -408,6 +409,7 @@ class EngineBuffer : public EngineObject {
     FRIEND_TEST(CueControlTest, SeekOnSetCueCDJ);
     FRIEND_TEST(CueControlTest, SeekOnSetCuePlay);
     CueControl* m_pCueControl;
+    Seek30Control* m_pSeek30Control;
 
     QList<EngineControl*> m_engineControls;
 
