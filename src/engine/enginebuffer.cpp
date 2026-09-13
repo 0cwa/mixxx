798e177f4795b2518f72cefce0b64076525fc977
0cwa <none@none>
2026-09-13 18:51:42 +0200

    Compose PR23 row: fix-optional-engine-enginebuffer-integration

--- src/engine/enginebuffer.cpp
+++ src/engine/enginebuffer.cpp
@@ -39,6 +39,14 @@
 #include "engine/bufferscalers/enginebufferscalerubberband.h"
 #endif
 
+#ifdef __BUNGEE__
+#include "engine/bufferscalers/enginebufferscalebungee.h"
+#endif
+
+#ifdef __SIGNALSMITH__
+#include "engine/bufferscalers/enginebufferscalesignalsmith.h"
+#endif
+
 #ifdef __VINYLCONTROL__
 #include "engine/controls/vinylcontrolcontrol.h"
 #endif
@@ -287,6 +295,12 @@ EngineBuffer::EngineBuffer(const QString& group,
 #ifdef __RUBBERBAND__
     m_pScaleRB = new EngineBufferScaleRubberBand(m_pReadAheadManager);
 #endif
+#ifdef __BUNGEE__
+    m_pScaleBungee = new EngineBufferScaleBungee(m_pReadAheadManager);
+#endif
+#ifdef __SIGNALSMITH__
+    m_pScaleSignalSmith = new EngineBufferScaleSignalSmith(m_pReadAheadManager);
+#endif
     slotKeylockEngineChanged(m_pKeylockEngine->get());
     m_pScaleVinyl = m_pScaleLinear;
     m_pScale = m_pScaleVinyl;
@@ -342,6 +356,12 @@ EngineBuffer::~EngineBuffer() {
     delete m_pScaleST;
 #ifdef __RUBBERBAND__
     delete m_pScaleRB;
+#endif
+#ifdef __BUNGEE__
+    delete m_pScaleBungee;
+#endif
+#ifdef __SIGNALSMITH__
+    delete m_pScaleSignalSmith;
 #endif
 
     delete m_pKeylock;
@@ -893,6 +913,16 @@ void EngineBuffer::slotKeylockEngineChanged(double dIndex) {
         m_pScaleKeylock = m_pScaleRB;
         break;
 #endif
+#ifdef __BUNGEE__
+    case KeylockEngine::Bungee:
+        m_pScaleKeylock = m_pScaleBungee;
+        break;
+#endif
+#ifdef __SIGNALSMITH__
+    case KeylockEngine::SignalSmith:
+        m_pScaleKeylock = m_pScaleSignalSmith;
+        break;
+#endif
     default:
         slotKeylockEngineChanged(static_cast<double>(defaultKeylockEngine()));
         break;
@@ -1244,6 +1274,12 @@ void EngineBuffer::process(CSAMPLE* pOutput, const std::size_t bufferSize) {
     m_pScaleST->setSignal(m_sampleRate, m_channelCount);
 #ifdef __RUBBERBAND__
     m_pScaleRB->setSignal(m_sampleRate, m_channelCount);
+#endif
+#ifdef __BUNGEE__
+    m_pScaleBungee->setSignal(m_sampleRate, m_channelCount);
+#endif
+#ifdef __SIGNALSMITH__
+    m_pScaleSignalSmith->setSignal(m_sampleRate, m_channelCount);
 #endif
 
     if (isTrackLoaded() && m_pause.tryLock()) {
--- src/engine/enginebuffer.h
+++ src/engine/enginebuffer.h
@@ -23,6 +23,14 @@
 #include "engine/bufferscalers/enginebufferscalerubberband.h"
 #endif
 
+#ifdef __BUNGEE__
+#include "engine/bufferscalers/enginebufferscalebungee.h"
+#endif
+
+#ifdef __SIGNALSMITH__
+#include "engine/bufferscalers/enginebufferscalesignalsmith.h"
+#endif
+
 //for the writer
 #ifdef __SCALER_DEBUG__
 #include <QFile>
@@ -91,6 +99,12 @@ class EngineBuffer : public EngineObject {
         RubberBandFiner = 2,
         RubberBandR3ShortWindow = 3,
 #endif
+#ifdef __BUNGEE__
+        Bungee = 4,
+#endif
+#ifdef __SIGNALSMITH__
+        SignalSmith = 5,
+#endif
     };
     Q_ENUM(KeylockEngine);
 
@@ -101,6 +115,12 @@ class EngineBuffer : public EngineObject {
             KeylockEngine::RubberBandFaster,
             KeylockEngine::RubberBandFiner,
             KeylockEngine::RubberBandR3ShortWindow,
+#endif
+#ifdef __BUNGEE__
+            KeylockEngine::Bungee,
+#endif
+#ifdef __SIGNALSMITH__
+            KeylockEngine::SignalSmith,
 #endif
     };
 
@@ -186,12 +206,20 @@ class EngineBuffer : public EngineObject {
             if (EngineBufferScaleRubberBand::isEngineFinerAvailable()) {
                 return tr("Rubberband R3 MW (slow, highest quality)");
             }
-            [[fallthrough]];
+            return tr("Rubberband (fast, medium quality)");
         case KeylockEngine::RubberBandR3ShortWindow:
             if (EngineBufferScaleRubberBand::isEngineFinerAvailable()) {
                 return tr("Rubberband R3 SW (fast, high quality)");
             }
-            [[fallthrough]];
+            return tr("Rubberband (fast, medium quality)");
+#endif
+#ifdef __BUNGEE__
+        case KeylockEngine::Bungee:
+            return tr("Bungee (high quality)");
+#endif
+#ifdef __SIGNALSMITH__
+        case KeylockEngine::SignalSmith:
+            return tr("Signalsmith Stretch (experimental)");
 #endif
         default:
 #ifdef __RUBBERBAND__
@@ -212,6 +240,14 @@ class EngineBuffer : public EngineObject {
         case KeylockEngine::RubberBandFiner:
         case KeylockEngine::RubberBandR3ShortWindow:
             return EngineBufferScaleRubberBand::isEngineFinerAvailable();
+#endif
+#ifdef __BUNGEE__
+        case KeylockEngine::Bungee:
+            return true;
+#endif
+#ifdef __SIGNALSMITH__
+        case KeylockEngine::SignalSmith:
+            return true;
 #endif
         default:
             return false;
@@ -474,6 +510,12 @@ class EngineBuffer : public EngineObject {
     EngineBufferScaleST* m_pScaleST;
 #ifdef __RUBBERBAND__
     EngineBufferScaleRubberBand* m_pScaleRB;
+#endif
+#ifdef __BUNGEE__
+    EngineBufferScaleBungee* m_pScaleBungee;
+#endif
+#ifdef __SIGNALSMITH__
+    EngineBufferScaleSignalSmith* m_pScaleSignalSmith;
 #endif
 
     // Indicates whether the scaler has changed since the last process()
