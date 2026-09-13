b85a92bff60856dcf96cdcfd65779bb9d2dd6ba8
0cwa <none@none>
2026-09-13 19:11:11 +0200

    Integrate EngineBuffer test placement

--- src/test/enginebuffertest.cpp
+++ src/test/enginebuffertest.cpp
@@ -50,15 +50,6 @@ class EngineBufferTest : public MockedEngineBackendTest {
 
 class EngineBufferE2ETest : public SignalPathTest {};
 
-TEST_F(EngineBufferTest, FractionalPlayposClampsToTrackBounds) {
-    EngineBuffer* pEngineBuffer = m_pChannel1->getEngineBuffer();
-    pEngineBuffer->m_trackEndPositionOld =
-            mixxx::audio::FramePos::fromEngineSamplePos(200.0);
-
-    EXPECT_DOUBLE_EQ(-0.01, pEngineBuffer->fractionalPlayposFromAbsolute(-1.0));
-    EXPECT_DOUBLE_EQ(0.5, pEngineBuffer->fractionalPlayposFromAbsolute(50.0));
-    EXPECT_DOUBLE_EQ(1.0, pEngineBuffer->fractionalPlayposFromAbsolute(101.0));
-}
 #ifdef __STEM__
 namespace {
 TrackPointer createStemTrack(int channelCount) {
@@ -367,6 +358,16 @@ TEST_F(EngineBufferTest, ProcessWithChannelLayoutClearsInvalidOutput) {
 }
 #endif
 #endif
+
+TEST_F(EngineBufferTest, FractionalPlayposClampsToTrackBounds) {
+    EngineBuffer* pEngineBuffer = m_pChannel1->getEngineBuffer();
+    pEngineBuffer->m_trackEndPositionOld =
+            mixxx::audio::FramePos::fromEngineSamplePos(200.0);
+
+    EXPECT_DOUBLE_EQ(-0.01, pEngineBuffer->fractionalPlayposFromAbsolute(-1.0));
+    EXPECT_DOUBLE_EQ(0.5, pEngineBuffer->fractionalPlayposFromAbsolute(50.0));
+    EXPECT_DOUBLE_EQ(1.0, pEngineBuffer->fractionalPlayposFromAbsolute(101.0));
+}
 
 TEST_F(EngineBufferTest, DisableKeylockResetsPitch) {
     // To prevent one-slider users from getting stuck on a key,
