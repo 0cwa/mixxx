e2ff6f0a4e4d8f233de734b84eb1e13540fa22af
0cwa <none@none>
2026-09-21 14:01:19 +0200

    Test Rekordbox cue type transitions
    Cover production cue-type updates and transition behavior through the importer path.

--- src/library/rekordbox/rekordboxfeature.cpp
+++ src/library/rekordbox/rekordboxfeature.cpp
@@ -1109,6 +1109,7 @@ void importHotCue(TrackPointer track,
             : mixxx::CueType::HotCue;
 
     if (pCue) {
+        pCue->setType(type);
         pCue->setStartAndEndPosition(startPosition, endPosition);
     } else {
         pCue = track->createAndAddCue(
